eb4ce48a6e4e9989116bf62f644283ba8e59c20d
soo8oct <min.redss@gmail.com>
2025-02-18 16:35:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 export default {
   async scheduled(event, env, ctx) {
-    // Google Apps Script Web App URL (배포 후 얻은 URL로 변경)
     const googleAppsScriptURL = "https://script.google.com/macros/s/AKfycbyfYt7APbNXl68mqHWLLqcYfQjJlktNj8XZONLB-tS7c6-UlE7Tp0d0O9o0dHdbimQB/exec";
 
     try {
